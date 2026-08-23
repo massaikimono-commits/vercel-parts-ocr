d769661eda0d8877466f7aeb8e18618490b11d81
massaikimono-commits <massa_ikimono@icloud.com>
2026-08-23 18:55:36 +0900

    Remove duplicate vehicle OCR correction mounts

--- app/vehicle-workflow-v2/layout.tsx
+++ app/vehicle-workflow-v2/layout.tsx
@@ -1,12 +1,3 @@
-import CertificateRowPriorityFix from "../certificate-row-priority-fix";
-import CertificateFuelClassificationFix from "../certificate-fuel-classification-fix";
-
 export default function VehicleWorkflowLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      {children}
-      <CertificateRowPriorityFix />
-      <CertificateFuelClassificationFix />
-    </>
-  );
+  return <>{children}</>;
 }
