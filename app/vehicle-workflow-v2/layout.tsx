1384ebd1f9ddf7ef8cf5f5467749ef805a03a9ea
massaikimono-commits <massa_ikimono@icloud.com>
2026-08-23 20:56:05 +0900

    Mount wide-band vehicle certificate OCR correction

--- app/vehicle-workflow-v2/layout.tsx
+++ app/vehicle-workflow-v2/layout.tsx
@@ -1,10 +1,12 @@
 import CertificateFulltextFix from "../certificate-fulltext-fix";
+import CertificateTopBandFix from "../certificate-top-band-fix";
 
 export default function VehicleWorkflowLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
       <CertificateFulltextFix />
+      <CertificateTopBandFix />
     </>
   );
 }
