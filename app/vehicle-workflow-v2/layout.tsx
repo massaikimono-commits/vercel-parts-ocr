75ea653addbd3362ea8019398cf460f0fbdd4d3a
massaikimono-commits <massa_ikimono@icloud.com>
2026-08-23 22:32:03 +0900

    Use final calibrated certificate OCR correction

--- app/vehicle-workflow-v2/layout.tsx
+++ app/vehicle-workflow-v2/layout.tsx
@@ -1,12 +1,12 @@
 import CertificateFulltextFix from "../certificate-fulltext-fix";
-import CertificateTopCellsFix from "../certificate-top-cells-fix";
+import CertificateFinalCalibration from "../certificate-final-calibration";
 
 export default function VehicleWorkflowLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
       <CertificateFulltextFix />
-      <CertificateTopCellsFix />
+      <CertificateFinalCalibration />
     </>
   );
 }
