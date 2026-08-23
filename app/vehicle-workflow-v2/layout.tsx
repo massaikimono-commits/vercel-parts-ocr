56aba8a0eb7321041133d192bc3f9ae92ac5b56b
massaikimono-commits <massa_ikimono@icloud.com>
2026-08-23 18:20:10 +0900

    Mount light oil fuel classification fix

--- app/vehicle-workflow-v2/layout.tsx
+++ app/vehicle-workflow-v2/layout.tsx
@@ -1,10 +1,12 @@
 import CertificateRowPriorityFix from "../certificate-row-priority-fix";
+import CertificateFuelClassificationFix from "../certificate-fuel-classification-fix";
 
 export default function VehicleWorkflowLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
       <CertificateRowPriorityFix />
+      <CertificateFuelClassificationFix />
     </>
   );
 }
