67e3b70f7d96cf3717c21b84eb077b94a1e05f7f
massaikimono-commits <massa_ikimono@icloud.com>
2026-08-24 15:30:04 +0900

    Show React state values beside certificate date fields

--- app/certificate-final-native-fix.jsx
+++ app/certificate-final-native-fix.jsx
@@ -2,12 +2,14 @@
 
 import CertificateAuthoritativeReaderV2 from "./certificate-authoritative-reader-v2";
 import CertificatePostOcrStateLock from "./certificate-post-ocr-state-lock";
+import CertificateReactPropsDebug from "./certificate-react-props-debug";
 
 export default function CertificateFinalNativeFix() {
   return (
     <>
       <CertificateAuthoritativeReaderV2 />
       <CertificatePostOcrStateLock />
+      <CertificateReactPropsDebug />
     </>
   );
 }
