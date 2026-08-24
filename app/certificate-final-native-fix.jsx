cfca0b3e8149a1811e582c07de4da091127abedd
massaikimono-commits <massa_ikimono@icloud.com>
2026-08-24 15:48:22 +0900

    Use v3 QR priority instead of external state lock

--- app/certificate-final-native-fix.jsx
+++ app/certificate-final-native-fix.jsx
@@ -1,14 +1,12 @@
 "use client";
 
 import CertificateAuthoritativeReaderV2 from "./certificate-authoritative-reader-v2";
-import CertificatePostOcrStateLock from "./certificate-post-ocr-state-lock";
 import CertificateReactPropsDebug from "./certificate-react-props-debug";
 
 export default function CertificateFinalNativeFix() {
   return (
     <>
       <CertificateAuthoritativeReaderV2 />
-      <CertificatePostOcrStateLock />
       <CertificateReactPropsDebug />
     </>
   );
