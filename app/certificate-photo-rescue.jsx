2dfd362c7e17efd3c7eee9fd83614bbe6f7f7246
massaikimono-commits <massa_ikimono@icloud.com>
2026-08-26 20:45:06 +0900

    feat: mount critical photo OCR rescue v2

--- app/certificate-photo-rescue.jsx
+++ app/certificate-photo-rescue.jsx
@@ -1,7 +1,13 @@
 "use client";
 
 import CertificatePhotoDerivedV2 from "./certificate-photo-derived-v2";
+import CertificatePhotoCriticalOcrV2 from "./certificate-photo-critical-ocr-v2";
 
 export default function CertificatePhotoRescue() {
-  return <CertificatePhotoDerivedV2 />;
+  return (
+    <>
+      <CertificatePhotoDerivedV2 />
+      <CertificatePhotoCriticalOcrV2 />
+    </>
+  );
 }
