1ef0973b622938b5e71314431d14cdd94c3d74d7
massaikimono-commits <massa_ikimono@icloud.com>
2026-09-15 19:30:54 +0900

    ux: isolate top vehicle registration as new mode

--- app/vehicle-workflow/page.tsx
+++ app/vehicle-workflow/page.tsx
@@ -1,5 +1,8 @@
 import { redirect } from "next/navigation";
 
-export default function VehicleWorkflowEntry() {
-  redirect("/vehicle-workflow-v2");
+type Props = { searchParams: Promise<{ mode?: string }> };
+
+export default async function VehicleWorkflowEntry({ searchParams }: Props) {
+  const { mode } = await searchParams;
+  redirect(mode === "new" ? "/vehicle-workflow-v2?mode=new" : "/vehicle-workflow-v2");
 }
